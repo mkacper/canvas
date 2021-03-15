c772cb8b368c8c9429184b1de5e86adf7b19049b
Kacper Mentel <mentel.kk@gmail.com>
2021-03-15 23:36:13 +0100

    Add flood fill

--- canvas.ex
+++ canvas.ex
@@ -64,6 +64,34 @@ defmodule Canvas do
     traverse_x(init, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
   end
 
+  # Flood fill
+  def flood_fill(p, fill_char, canvas) do
+    check_points(neighbours(p), %{}, fill_char, canvas)
+  end
+
+  defp check_points([], _, _, canvas), do: canvas
+
+  defp check_points([{x, y} = p | ps], visited, fill_char, canvas) when x > 32 or y > 32 or x < 0 or y < 0,
+    do: check_points(ps, Map.put(visited, p, :visited), fill_char, canvas)
+
+  defp check_points([p | ps], visited, fill_char, canvas) when is_map_key(visited, p),
+    do: check_points(ps, visited, fill_char, canvas)
+
+  defp check_points([p | ps], visited, fill_char, canvas) when is_map_key(canvas, p),
+    do: check_points(ps, Map.put(visited, p, :visited), fill_char, canvas)
+
+  defp check_points([p | ps], visited, fill_char, canvas) do
+    canvas = Map.put(canvas, p, fill_char)
+    visited = Map.put(visited, p, :visited)
+    check_points(ps ++ neighbours(p), visited, fill_char, canvas)
+  end
+
+  defp neighbours({x, y}) do
+    [{x-1,y-1}, {x-1, y}, {x-1, y+1}, {x, y-1}, {x, y+1}, {x+1, y-1}, {x+1, y}, {x+1, y+1}]
+  end
+
+  # Helpers
+
   def to_binary(canvas) do
     for y <- 0..31 do
       Enum.join(for x <- 0..31 do
