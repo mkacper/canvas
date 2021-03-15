5bd73395b5300c7cf626d1ffde4e44e496581a27
Kacper Mentel <mentel.kk@gmail.com>
2021-03-15 22:51:39 +0100

    Improve code style

--- canvas.ex
+++ canvas.ex
@@ -17,55 +17,46 @@ defmodule Canvas do
 
 
   def draw_rectangle2({x, y}, width, height, outline_char, fill_char \\ nil, canvas \\ %{}) do
+    init = {x, y}
+    current = init
     max_x = x+width-1
     max_y = y+height-1
-    traverse_y({x, y}, {x, y}, max_x, max_y, outline_char, fill_char, canvas)
+    traverse_y(init, current, max_x, max_y, outline_char, fill_char, canvas)
   end
 
   defp traverse_y(_init, {_cx, cy}, _max_x, max_y, _outline_char, _fill_char, canvas) when cy > max_y,
     do: canvas
 
-  defp traverse_y(init, {cx, max_y = cy}, max_x, max_y, outline_char, fill_char, canvas),
-    do: traverse_x(init, {cx, cy}, max_x, max_y, outline_char, fill_char, canvas)
-
-  defp traverse_y(init, {cx, cy}, max_x, max_y, outline_char, fill_char, canvas) do
-    canvas = traverse_x(init, {cx, cy}, max_x, max_y, outline_char, fill_char, canvas)
+  defp traverse_y(init, {cx, cy} = current, max_x, max_y, outline_char, fill_char, canvas) do
+    canvas = traverse_x(init, current, max_x, max_y, outline_char, fill_char, canvas)
     traverse_y(init, {cx, cy + 1}, max_x, max_y, outline_char, fill_char, canvas)
   end
 
-  defp traverse_x(_init, {cx = max_x, cy}, max_x, _max_y, outline_char, _fill_char, canvas) do
-    IO.inspect "done"
-    Map.put(canvas, {cx, cy}, outline_char) 
+  defp traverse_x(_init, {_cx = max_x, _} = current, max_x, _max_y, outline_char, _fill_char, canvas) do
+    Map.put(canvas, current, outline_char) 
   end
 
-  defp traverse_x({x, y}, {cx, cy = max_y}, max_x, max_y, outline_char, fill_char, canvas) do
-    IO.inspect "max y"
+  defp traverse_x(init, {cx, cy = max_y}, max_x, max_y, outline_char, fill_char, canvas) do
     canvas = Map.put(canvas, {cx, cy}, outline_char) 
-    traverse_x({x, y}, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
+    traverse_x(init, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
   end
 
-  defp traverse_x({x, y}, {cx, y = cy}, max_x, max_y, outline_char, fill_char, canvas) do
-    IO.inspect "STEP 1"
-    IO.inspect cx
-    IO.inspect max_x
-    canvas = Map.put(canvas, {cx, cy}, outline_char) 
-    traverse_x({x, y}, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
+  defp traverse_x({_, y} = init, {cx, y = cy} = current, max_x, max_y, outline_char, fill_char, canvas) do
+    canvas = Map.put(canvas, current, outline_char) 
+    traverse_x(init, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
   end
 
-  defp traverse_x({x, y}, {x = cx, cy}, max_x, max_y, outline_char, fill_char, canvas) do
-    IO.inspect "2"
-    canvas = Map.put(canvas, {cx, cy}, outline_char) 
-    traverse_x({x, y}, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
+  defp traverse_x({x, _} = init, {x = cx, cy} = current, max_x, max_y, outline_char, fill_char, canvas) do
+    canvas = Map.put(canvas, current, outline_char) 
+    traverse_x(init, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
   end
 
-  defp traverse_x({x, y}, {cx, cy}, max_x, max_y, outline_char, fill_char, canvas) when not is_nil(fill_char) do
-    IO.inspect "3"
-    canvas = Map.put(canvas, {cx, cy}, fill_char) 
-    traverse_x({x, y}, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
+  defp traverse_x(init, {cx, cy} = current, max_x, max_y, outline_char, fill_char, canvas) when not is_nil(fill_char) do
+    canvas = Map.put(canvas, current, fill_char) 
+    traverse_x(init, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
   end
 
   defp traverse_x(init, {cx, cy}, max_x, max_y, outline_char, fill_char, canvas) do
-    IO.inspect "4"
     traverse_x(init, {cx + 1, cy}, max_x, max_y, outline_char, fill_char, canvas)
   end
 
