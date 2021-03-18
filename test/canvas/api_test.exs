9440e0d428977625efd195ae36f6ddcda4b93108
Kacper Mentel <mentel.kk@gmail.com>
2021-03-19 00:01:13 +0100

    Add tests for rectangle drawing endpoint

--- test/canvas/api_test.exs
+++ test/canvas/api_test.exs
@@ -49,6 +49,96 @@ defmodule Canvas.DrawerTest do
     assert resp_body =~ "Canvas not found"
   end
 
+  describe "Test drawing endpoint:" do
+    setup do
+      canvas = Drawer.new_canvas()
+      Drawer.save_canvas(canvas)
+      {:ok, canvas: canvas}
+    end
+
+    test "POST #{@canvas_path}/:id/drawings creates a rectangle drawing", %{canvas: canvas} do
+      [{x, y}, width, height, outline_char, fill_char] = drawing_fixture_spec()
+
+      params = %{
+        type: "rectangle",
+        init_point: [x, y],
+        width: width,
+        height: height,
+        outline_char: outline_char,
+        fill_char: fill_char
+      }
+
+      # WHEN
+      {status, resp_body} =
+        request(:post, Path.join(@canvas_path, "#{canvas.id}/drawings"), params)
+
+      # THEN
+      assert 201 = status
+      assert resp_body =~ "Drawing added successfully"
+
+      {200, canvas_bin} = request(:get, Path.join(@canvas_path, "#{canvas.id}"), nil)
+      assert canvas_bin =~ drawing_fixture()
+
+      # cleanup
+      :mnesia.clear_table(Canvas)
+      :mnesia.clear_table(Draw)
+    end
+
+    test "POST #{@canvas_path}/:id/drawings returns not found error when passed canvas id does not exist" do
+      [{x, y}, width, height, outline_char, fill_char] = drawing_fixture_spec()
+
+      params = %{
+        type: "rectangle",
+        init_point: [x, y],
+        width: width,
+        height: height,
+        outline_char: outline_char,
+        fill_char: fill_char
+      }
+
+      # WHEN
+      {status, resp_body} = request(:post, Path.join(@canvas_path, "12345/drawings"), params)
+
+      # THEN
+      assert 404 = status
+      assert resp_body =~ "Canvas not found"
+    end
+
+    @rectangle_drawing_params %{
+      type: "rectangle",
+      init_point: [1, 2],
+      width: 2,
+      height: 2,
+      outline_char: "@",
+      fill_char: "O"
+    }
+
+    for {error, params} <- [
+          {:unknown_type, %{@rectangle_drawing_params | type: "wrong_type"}},
+          {:width_not_integer, %{@rectangle_drawing_params | width: "1"}},
+          {:outline_and_fill_chars_null,
+           %{@rectangle_drawing_params | outline_char: nil, fill_char: nil}},
+          {:outline_char_not_string, %{@rectangle_drawing_params | outline_char: 1}},
+          {:outline_char_too_long, %{@rectangle_drawing_params | outline_char: "@@"}}
+        ] do
+      test "POST #{@canvas_path}/:id/drawings returns bad request error when" <>
+             " trying to create a rectangle drawing with bad params (#{error})",
+           %{canvas: canvas} do
+        # WHEN
+        {status, resp_body} =
+          request(
+            :post,
+            Path.join(@canvas_path, "#{canvas.id}/drawings"),
+            unquote(Macro.escape(params))
+          )
+
+        # THEN
+        assert 400 = status
+        assert resp_body =~ "Bad request"
+      end
+    end
+  end
+
   # Utils
 
   defp request(method, path, params) do
